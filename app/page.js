d03444b26848e41ba5bd989d5f02f0691053da52
omer.ozturk <ofozturk@eskisehir.edu.tr>
2023-08-27 17:59:55 +0300

    feat: create next app

---
+++ app/page.js
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className=" flex justify-center items-center h-screen w-full bg-red-600 text-white">
+      <p className='text-7xl font-bold'>Pokedex App</p>
+    </div>
+  )
+}
