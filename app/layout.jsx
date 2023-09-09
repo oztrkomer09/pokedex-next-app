9300980486bace5d22a417231a6d5dd7e904e279
omer.ozturk <ofozturk@eskisehir.edu.tr>
2023-09-10 01:07:00 +0300

    style: change style of main page and add link to page head

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,14 +1,17 @@
-import './globals.css'
-
+import "./globals.css";
+import Link from "next/link";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-
       <body className=" flex flex-col items-center h-screen w-full bg-red-600 px-24 py-5">
-        <h1 className='text-6xl font-tilt text-white mb-4'>Pokedex App</h1>
+        <Link href={"/"}>
+          <h1 className="sm:text-6xl text-4xl font-tilt text-white mb-4">
+            Pokedex App
+          </h1>
+        </Link>
         {children}
       </body>
     </html>
-  )
+  );
 }
