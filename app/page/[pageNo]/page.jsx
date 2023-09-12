6cdc30267a9112260e31abc802b8802d38f18b0a
omer.ozturk <ofozturk@eskisehir.edu.tr>
2023-09-12 15:48:57 +0300

    style(margin): delete unnecessary margin

--- app/page/[pageNo]/page.jsx
+++ app/page/[pageNo]/page.jsx
@@ -6,7 +6,7 @@ export default async function Page({ params }) {
   const pokemons = await getPokemons(params.pageNo);
 
   return (
-    <div className="bg-white w-full h-max flex flex-wrap justify-center items-center gap-2 p-12 mb-5 relative">
+    <div className="bg-white w-full h-max flex flex-wrap justify-center items-center gap-2 p-12 relative">
       {pokemons.results.map((pokemon) => (
         <PokemonCard key={pokemon.name} pokemon={pokemon} />
       ))}
