829a98da3b434ce8b9f68bc7f713134dfa1b614c
omer.ozturk <ofozturk@eskisehir.edu.tr>
2023-09-12 15:48:37 +0300

    feat: create loading element

---
+++ app/page/[pageNo]/loading.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const loading = () => {
+  return (
+    <div className="bg-white w-full h-full min-h-[520px] flex flex-wrap justify-center items-center ">
+      <p className="text-xl sm:text-4xl text-red-600 font-bold">Loading...</p>
+    </div>
+  );
+};
+
+export default loading;
