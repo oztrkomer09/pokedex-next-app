6a2d11cb54796f51ab607800c271c537c8a0ba79
omer.ozturk <ofozturk@eskisehir.edu.tr>
2023-09-12 15:26:42 +0300

    refactor(API): change getPokemons request to fetch items according to pageNo

--- app/API/pokemonAPI.js
+++ app/API/pokemonAPI.js
@@ -1,7 +1,8 @@
 const POKEMON_API = 'https://pokeapi.co/api/v2/pokemon/';
 
-export const getPokemons = async () => {
-    const response = await fetch(`${POKEMON_API}?limit=10`);
+export const getPokemons = async (pageNo) => {
+    const offsetNumber = pageNo === '1' ? 0 : (pageNo - 1) * 10;
+    const response = await fetch(`${POKEMON_API}?offset=${offsetNumber}&limit=10`);
     const data = await response.json();
     return data;
 };
